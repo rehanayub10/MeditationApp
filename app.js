7e8ec459b9d3599ab06805e5ac06447bf0cc75ef
Rehan Ayub <rehanayub20@gmail.com>
2020-05-05 23:05:14 -0400

    Added final changes

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = () => {
 
     //Time Display
     const timeDisplay = document.querySelector('.time-display');
+    const timeSelect = document.querySelectorAll('.time-select button');
 
     //Get length of outline
     const outlineLength = outline.getTotalLength();
@@ -17,13 +18,30 @@ const app = () => {
     //Duration
     let duration = 600;
 
-    // outline.style.strokeDasharray = 100;
+    //outline.style.strokeDasharray = 100;
     outline.style.strokeDasharray = outlineLength;
-    //outline.style.strokeDashoffset = 200;
+    outline.style.strokeDashoffset = outlineLength;
     
     //play sound
     play.addEventListener('click', () => {
         checkPlaying(song);
+    });
+
+    //select time
+    timeSelect.forEach(option => {
+        option.addEventListener('click', function() { //traditional formatting to use "this" keyword
+            duration = this.getAttribute('data-time');
+            //timeDisplay.textContent = `${Math.floor(duration/60)}:${Math.floor(duration % 60)}`;
+        });
+    });
+
+    //pick different songs/videos
+    sounds.forEach(sound => {
+        sound.addEventListener('click', function(){
+            song.src = this.getAttribute('data-sound');
+            video.src = this.getAttribute('data-video');
+            checkPlaying(song);
+        });
     });
 
     //Create a function specific to stop and play the sounds
@@ -39,11 +57,26 @@ const app = () => {
         }
     };
 
-    //We can animate the circle
+    //Animate the circle
     song.ontimeupdate = () => {
         let currentTime = song.currentTime;
+        //console.log(currentTime);
         let elapsed = duration - currentTime;
-        let seconds = elapsed % 60;
+        let seconds = Math.floor(elapsed % 60);
+        let minutes = Math.floor(elapsed/60);
+
+        let progress = outlineLength - (currentTime / duration) * outlineLength;
+        outline.style.strokeDashoffset = progress;
+
+        //Animate the text
+        timeDisplay.textContent = `${minutes}:${seconds}`;
+
+        if (currentTime >= duration) {
+            song.pause();
+            video.pause();
+            currentTime = 0;
+            play.src = './svg/play.svg';
+        }
     }
 };
 
